3680e5013e8fa3379f48d56df0dffe6ca1f60b8f
hemanth <hemanthbysani2002@gmail.com>
2022-10-24 18:03:56 +0530

    final edit to try.py

--- try.py
+++ try.py
@@ -12,15 +12,17 @@ class audio:
     recording = False
     file_exists = False
 
+    def __init__(self, name):
+        self.file_name = name
 
     def callback(self, indata, frames, time, status):
         self.q.put(indata.copy())
 
 
-    def threading_rec(self, x, name):
+    def threading_rec(self, x):
         if x == 1:
             #If recording is selected, then the thread is activated
-            t1=threading.Thread(target = self.record_audio(name))
+            t1=threading.Thread(target = self.record_audio)
             t1.start()
         elif x == 2:
             #To stop, set the flag to false
@@ -31,7 +33,7 @@ class audio:
             #To play a recording, it must exist.
             if file_exists:
                 #Read the recording if it exists and play it
-                data, fs = sf.read(str(name), dtype='float32')
+                data, fs = sf.read(self.file_name, dtype='float32')
                 sd.play(data,fs)
                 sd.wait()
             else:
@@ -39,7 +41,7 @@ class audio:
                 messagebox.showerror(message="Record something to play")
 
 
-    def record_audio(self,name):
+    def record_audio(self):
         #Declare global variables   
         global recording
         #Set to True to record
@@ -47,7 +49,7 @@ class audio:
         global file_exists
         #Create a file to save the audio
         messagebox.showinfo(message="Recording Audio. Speak into the mic")
-        with sf.SoundFile(str(name), mode='w', samplerate=44100,
+        with sf.SoundFile(self.file_name, mode='w', samplerate=44100,
                             channels=2) as file:
         #Create an input stream to record audio without a preset time
                 with sd.InputStream(samplerate=44100, channels=2, callback=self.callback):
@@ -62,19 +64,19 @@ class audio:
 class main:
 
     def __init__(self,name):
-        aud = audio()
+        aud = audio(name)
         voice_rec = Tk()
         voice_rec.geometry("360x200")
         voice_rec.title("Voice Recorder")
         voice_rec.config(bg="#107dc2")
-        title_lbl  = Label(voice_rec, text="Voice Recorder", bg="#107dc2").grid(row=0, column=0, columnspan=3)
+        title_lbl  = Label(voice_rec, text=" Voice Recorder", bg="#107dc2").grid(row=0, column=0, columnspan=3)
 
         #Button to record audio
-        record_btn = Button(voice_rec, text="Record Audio", command=lambda m=1:aud.threading_rec(m,name))
+        record_btn = Button(voice_rec, text="Record Audio", command=lambda m=1:aud.threading_rec(m))
         #Stop button
-        stop_btn = Button(voice_rec, text="Stop Recording", command=lambda m=2:aud.threading_rec(m,name))
+        stop_btn = Button(voice_rec, text="Stop Recording", command=lambda m=2:aud.threading_rec(m))
         #Play button
-        play_btn = Button(voice_rec, text="Play Recording", command=lambda m=3:aud.threading_rec(m,name))
+        play_btn = Button(voice_rec, text="Play Recording", command=lambda m=3:aud.threading_rec(m))
         #Position buttons
         record_btn.grid(row=1,column=1)
         stop_btn.grid(row=1,column=0)
@@ -82,5 +84,5 @@ class main:
         voice_rec.mainloop()
 
 if __name__ == '__main__':
-    x = 'q1.wav'
-    main(x)
+    name = "Q1.wav"
+    main(name)
