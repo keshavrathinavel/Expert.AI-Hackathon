53d18d27e2ba5858604e91ffff364b0c3fb4ca2b
Indira Kumar A K <indirakumarak@gmail.com>
2022-10-25 18:28:45 +0530

    speech to text with audio file

--- speech.py
+++ speech.py
@@ -14,8 +14,30 @@ def speech():
     except sr.UnknownValueError:
         print("Audio was unclear")
     except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
+        print("API error; {0}".format(e))
 
 
     return data
+
+def speech_using_audio_file(name):
+    import speech_recognition as sr
+    from os import path
+    file_name = str(name) + ".wav"
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), file_name)
+
+
+    # use the audio file as the audio source
+    r = sr.Recognizer()
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source)  # read the entire audio file
+
+    # recognize speech using recognize_google
+    try:
+        data = r.recognize_google(audio)
+        print("The text of the audio is:" + data)
+    except sr.UnknownValueError:
+        print("Audio was unclear")
+    except sr.RequestError as e:
+        print("API error; {0}".format(e))
+speech_using_audio_file("Q1")
 
